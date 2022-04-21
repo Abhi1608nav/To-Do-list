22a96691f6b9dd1817e6c891a8c1da16d21d054c
Abhi1608nav <97542210+Abhi1608nav@users.noreply.github.com>
2022-04-21 11:16:14 +0530

    Add files via upload

---
+++ assests/JS/index.js
@@ -0,0 +1,24 @@
+//getting started...
+$("ul").on("click","li",function(){
+    $(this).toggleClass("complete");
+    
+})
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    })
+    event.stopPropagation();
+})
+$("input[type='text']").keypress(function(event){
+    if(event.which===13)
+    {
+       var todos= $(this).val();
+       $(this).val("");
+        $("ul").append("<li>"+"<span><i class='fa-solid fa-trash-can'></i></span>"+" "+todos+"</li>");
+    }
+    
+    
+})
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+})
